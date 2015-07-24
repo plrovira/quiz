9f54ff318260971e600979cdcf7b0e481f8fc055
Pere Lluís Rovira <pll.rovira@edn.cat>
2015-07-24 08:08:33 +0200

    Múltiples preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,14 +1,31 @@
 var models = require('../models/models.js');
 
-// GET /quizes/question
+
+// GET /quizes  muestra preguntas
 exports.question = function(req, res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs', {quizes: quizes});
+	})
+
+// GET /quizes/question  una sola pregunta
+
+/*exports.question = function(req, res){
 	models.Quiz.findAll().then(function(quiz){
 		res.render('quizes/question', {pregunta: quiz[0].pregunta});
 	})
+};*/
+
+// GET /quizes/:id  varias preguntas
+
+exports.show = function(req, res){
+	models.Quiz.find(req.params.quiId).then(function(quiz){
+		res.render('quizes/show', {quiz: quiz});
+	})
 };
 
-// GET  /quizes/answer
-exports.answer = function(req, res){
+
+// GET  /quizes/answer  una sola pregunta/respuesta
+/*exports.answer = function(req, res){
 	models.Quiz.findAll().then(function(quiz){
 		if (req.query.respuesta === quiz[0].respuesta){
 			res.render('quizes/answer',{respuesta: 'Correcto'});
@@ -16,7 +33,21 @@ exports.answer = function(req, res){
 			res.render('quizes/answer',{respuesta: 'Incorrecto'});
 		}
 	})
+};*/
+
+exports.answer = function(req, res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		if (req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer',{quiz: quiz, respuesta: 'Correcto'});
+		} else{
+			res.render('quizes/answer',{quiz: quiz, respuesta: 'Incorrecto'});
+		}
+	})
 };
+
+
+
+
 
 // GET /author
 exports.author = function(req, res){
