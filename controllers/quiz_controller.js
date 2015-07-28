b37e502bb6fab20aaa74f525567802bd20d7780d
Pere Lluís Rovira <pll.rovira@edn.cat>
2015-07-28 18:40:02 +0200

    Validación de datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -17,17 +17,25 @@ exports.new = function(req, res){
 	var quiz = models.Quiz.build(  // crea objeto quiz
 		{ pregunta: "Pregunta", respuesta: "Respuesta"}
 	);
-	res.render('quizes/new', {quiz: quiz});
+	res.render('quizes/new', {quiz: quiz, errors: []});
 };
 
 // POST  /quizes/create
 exports.create = function(req, res){
 	var quiz = models.Quiz.build( req.body.quiz);
 
-	// guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) // Redireccion HTTP (URL relativo) lista de preguntas
+	quiz.validate()
+	  .then(
+		function(err){
+	  	  if (err){
+			res.render('quizes/new',{quiz: quiz, errors: err.errors});
+		  } else {
+			quiz // guarda en DB los campos pregunta y respuesta de quiz
+			.save({fields: ["pregunta", "respuesta"]})
+			.then(function(){res.redirect('/quizes')});
+		  } // Redireccion HTTP (URL relativo) lista de preguntas
+	  	}
+	  );
 };
 
 
@@ -35,7 +43,7 @@ exports.create = function(req, res){
 // GET /quizes  muestra preguntas
 exports.index = function(req, res){
 	models.Quiz.findAll().then(function(quizes){
-		res.render('quizes/index', {quizes: quizes});
+		res.render('quizes/index', {quizes: quizes, errors: []});
 	}).catch(function(error) {next(error);})
 };
 
@@ -50,7 +58,7 @@ exports.index = function(req, res){
 // GET /quizes/:id  varias preguntas
 
 exports.show = function(req, res){
-	res.render('quizes/show', {quiz: req.quiz});
+	res.render('quizes/show', {quiz: req.quiz, errors: []});
 };
 
 
@@ -70,11 +78,13 @@ exports.answer = function(req, res){
 	if (req.query.respuesta === req.quiz.respuesta){
 			resultado = 'Correcto';
 	}
-	res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
+	res.render('quizes/answer',
+				{quiz: req.quiz, 
+				 respuesta: resultado,
+				 errors: []
+				}
+	);
 };
-
-
-
 
 
 // GET /author
--- models/quiz.js
+++ models/quiz.js
@@ -1,11 +1,25 @@
 // Definición del modelo de QUIZ
 
-module.exports = function(sequelize, DataTypes){
+/*module.exports = function(sequelize, DataTypes){
 	return sequelize.define('Quiz', 
 		{ pregunta: DataTypes.STRING,
 		  respuesta: DataTypes.STRING,
 		});
+}*/
 
+// Definición del modelo Quiz con validación
+module.exports = function(sequelize, DataTypes){
+	return sequelize.define(
+	  'Quiz', 
+	  { pregunta: {
+			type: DataTypes.STRING,
+			validate: { notEmpty: {msg: "-> falta Pregunta"}}
+		},
+		  respuesta: {
+			type: DataTypes.STRING,
+			validate: { notEmpty: {msg: "-> falta Respuesta"}}
+		}
+	  }
+	);
+};
 
-
-}
